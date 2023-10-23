97105e12f830305cc0cb65e22dcd17acb82015df
Camila Guerra <101744274+Cmilag@users.noreply.github.com>
2023-10-23 10:52:41 -0300

    Actualización Qr Duoc 23-10 v4

--- src/app/pages/hub/hub.page.ts
+++ src/app/pages/hub/hub.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+import { IonicSlides } from '@ionic/angular';
+import { __await } from 'tslib';
 
 @Component({
   selector: 'app-hub',
@@ -8,9 +11,10 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['./hub.page.scss'],
 })
 export class HubPage implements OnInit {
-
-  constructor(private menuController: MenuController,
-    private router: Router) { }
+  
+  handlerMessage='';
+  roleMessage='';
+  constructor(private menuController: MenuController, private alertController: AlertController, private router: Router) { }
 
   ngOnInit() {
   }
@@ -24,4 +28,26 @@ export class HubPage implements OnInit {
     return sessionStorage.removeItem('usuario')
     return sessionStorage.removeItem('isactive')
   }
-}
+
+  async mensaje(){
+    if(localStorage.getItem('ingresado')=='true')
+    {
+      const alert = await this.alertController.create({
+        header: 'Bienvenido a TeLLevoApp ' + localStorage.getItem('nombre'),
+        buttons: [
+          {
+            text: 'Gracias',
+            role: 'confirm',
+            handler: () => {
+              this.handlerMessage = 'disfrute nuestro servicio';
+            },
+          },
+        ],
+      });
+
+      await alert.present();
+
+      const { role } = await alert.onDidDismiss();
+      this.roleMessage = `Dismissed with role: ${role}`;
+    }
+  }}
